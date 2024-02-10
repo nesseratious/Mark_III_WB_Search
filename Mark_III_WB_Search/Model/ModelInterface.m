571122d728a8fabdd01867a2c1b928e42338dda9
lekarion <lekarion.dev@gmail.com>
2024-02-11 00:39:04 +0200

    feat: model implementation started

---
+++ Mark_III_WB_Search/Model/ModelInterface.m
@@ -0,0 +1,12 @@
+//
+//  ModelInterface.m
+//  Mark_III_WB_Search
+//
+//  Created by developer on 11.02.2024.
+//
+
+#import "ModelInterface.h"
+
+const NSInteger nextEventsFetchingYearsCount = 10;
+const NSInteger initialEventsFetchingPastYearsCount = 5;
+const NSInteger initialEventsFetchingFutureYearsCount = 5;
